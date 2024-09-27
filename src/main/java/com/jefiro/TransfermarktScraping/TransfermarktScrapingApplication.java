c80985ff4f787bece3a89a2454de8afa309b2645
JEFIRO <150871603+JEFIRO@users.noreply.github.com>
2024-09-26 23:44:38 -0300

    commit inicial

---
+++ src/main/java/com/jefiro/TransfermarktScraping/TransfermarktScrapingApplication.java
@@ -0,0 +1,13 @@
+package com.jefiro.TransfermarktScraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfermarktScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfermarktScrapingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefiro/TransfermarktScraping/TransfermarktScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefiro.TransfermarktScraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfermarktScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
